e4da72d166db7c2b5ad1d2edca1a81393ffacec5
霁月安澜 <cloudfirer@gmail.com>
2025-07-13 06:32:57 +0800

    只提交Part2

---
+++ patrialPush.py
@@ -0,0 +1,8 @@
+keys = ['name', 'age', 'city']
+values = ['Alice', 25, 'New York']
+# zip(keys, values) 会把 keys 和 values 这两个列表 “压缩” 到一起，形成由元组构成的迭代器。
+# dict 接收可迭代对象，进而创建字典；取第一个元素为键，第二个元素为值
+dictionary = dict(zip(keys, values))
+print(dictionary)
+
+print("Part 2")
